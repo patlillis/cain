5891aaf640359a3c7dc33304e7f43ee451108342
Pat Lillis <lillispm@gmail.com>
2016-06-14 21:24:53 -0400

    add control point indicators

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//http://threejs.org/examples/webgl_lines_sphere.html
+
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
@@ -11,14 +13,17 @@ var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 
 camera.position.z = 20;
 
-//Create a closed bent a sine-like wave
-var curve = new THREE.CatmullRomCurve3( [
+var controlPoints = [
 	new THREE.Vector3( -10, 0, 10 ),
 	new THREE.Vector3( -5, 5, 5 ),
 	new THREE.Vector3( 0, 0, 0 ),
 	new THREE.Vector3( 5, -5, 5 ),
 	new THREE.Vector3( 10, 0, 10 )
-	] );
+];
+
+
+//Create a closed bent a sine-like wave
+var curve = new THREE.CatmullRomCurve3( controlPoints );
 
 curve.closed = true;
 
@@ -31,6 +36,18 @@ var material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
 var splineObject = new THREE.Line( geometry, material );
 
 scene.add(splineObject);
+
+for (var i = 0; i < controlPoints.length; i++) {
+	var vert = controlPoints[i];
+
+	var geometry = new THREE.SphereGeometry(.1, 50, 50);
+	geometry.translate(vert.x, vert.y, vert.z);
+
+	var material = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+	var sphere = new THREE.Mesh(geometry, material);
+	scene.add(sphere);
+}
+
 
 var render = function () {
 	requestAnimationFrame( render );
