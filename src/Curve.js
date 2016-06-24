400ebcba6ac92b414c0fa2990733fbf4fec710d4
Pat Lillis <lillispm@gmail.com>
2016-06-24 09:08:42 -0400

    Remove some extraneous comments

--- src/Curve.js
+++ src/Curve.js
@@ -9,10 +9,6 @@ export default class Curve {
 		//Create a closed bent a sine-like wave
 		this.catmullRom = new THREE.CubicBezierCurve3( this.points[0], this.points[1], this.points[2], this.points[3] );
 
-		// this.closed = true;
-
-		// this.catmullRom.closed = closed;
-
 		this.geometry = new THREE.Geometry();
 		this.geometry.vertices = this.catmullRom.getPoints( 100 );
 
@@ -21,31 +17,6 @@ export default class Curve {
 		//Create the final Object3d to add to the scene
 		this.splineObject = new THREE.Line( this.geometry, this.material );
 	}
-
-	// get closed() {
-	// 	return this.catmullRom.closed;
-	// }
-
-	// set closed(val) {
-	// 	if (val !== this.catmullRom.closed) {
-	// 		//Need to update geometry as well.
-	// 		this.catmullRom.closed = val;
-
-	// 		this.geometry = new THREE.Geometry();
-	// 		this.geometry.vertices = this.catmullRom.getPoints( 100 );
-
-	// 		//Create the final Object3d to add to the scene
-	// 		var newSpline = new THREE.Line( this.geometry, this.material );
-
-	// 		//Add new object to scene
-	// 		if (this.scene !== undefined) {
-	// 			this.scene.remove(this.splineObject);
-	// 			this.scene.add(newSpline);
-	// 		}
-
-	// 		this.splineObject = newSpline;
-	// 	}
-	// }
 
 	addToScene(scene) {
 		this.scene = scene;
