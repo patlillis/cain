fed0c9a6a28d6be61c36563e1710a8307c3a855d
Pat Lillis <lillispm@gmail.com>
2016-06-25 17:03:13 -0400

    Add more curves

--- src/Curve.js
+++ src/Curve.js
@@ -7,45 +7,16 @@ export default class Curve {
 		this.points = points;
 
 		//Create a closed bent a sine-like wave
-		this.catmullRom = new THREE.CubicBezierCurve3( this.points[0], this.points[1], this.points[2], this.points[3] );
-
-		// this.closed = true;
-
-		// this.catmullRom.closed = closed;
+		this.bezier = new THREE.CubicBezierCurve3( this.points[0], this.points[1], this.points[2], this.points[3] );
 
 		this.geometry = new THREE.Geometry();
-		this.geometry.vertices = this.catmullRom.getPoints( 100 );
+		this.geometry.vertices = this.bezier.getPoints( 100 );
 
 		this.material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
 
 		//Create the final Object3d to add to the scene
 		this.splineObject = new THREE.Line( this.geometry, this.material );
 	}
-
-	// get closed() {
-	// 	return this.catmullRom.closed;
-	// }
-
-	// set closed(val) {
-	// 	if (val !== this.catmullRom.closed) {
-	// 		//Need to update geometry as well.
-	// 		this.catmullRom.closed = val;
-
-	// 		this.geometry = new THREE.Geometry();
-	// 		this.geometry.vertices = this.catmullRom.getPoints( 100 );
-
-	// 		//Create the final Object3d to add to the scene
-	// 		var newSpline = new THREE.Line( this.geometry, this.material );
-
-	// 		//Add new object to scene
-	// 		if (this.scene !== undefined) {
-	// 			this.scene.remove(this.splineObject);
-	// 			this.scene.add(newSpline);
-	// 		}
-
-	// 		this.splineObject = newSpline;
-	// 	}
-	// }
 
 	addToScene(scene) {
 		this.scene = scene;
--- src/Organic.js
+++ src/Organic.js
@@ -11,9 +11,37 @@ export default class Organic {
 			new THREE.Vector3(30, 20),
 			new THREE.Vector3(50, -10)
 		]));
+
+		this.curves.push(new Curve([
+			new THREE.Vector3(23, 3),
+			new THREE.Vector3(0, -20),
+			new THREE.Vector3(-20, -20),
+			new THREE.Vector3(-40, -5)
+		]));
+
+		this.curves.push(new Curve([
+			new THREE.Vector3(23, 3),
+			new THREE.Vector3(0, -20),
+			new THREE.Vector3(-20, -20),
+			new THREE.Vector3(-45, -40)
+		]));
+
+		var material = new THREE.MeshBasicMaterial({
+			color: 0x0000ff
+		});
+
+		var radius = 3;
+		var segments = 32; //<-- Increase or decrease for more resolution I guess
+
+		var circleGeometry = new THREE.CircleGeometry(radius, segments);
+
+		circleGeometry.translate(23, 3, 0);
+
+		this.circle = new THREE.Mesh(circleGeometry, material);
 	}
 
 	addToScene(scene) {
 		this.curves.forEach(c => c.addToScene(scene));
+		scene.add(this.circle);
 	}
 }
